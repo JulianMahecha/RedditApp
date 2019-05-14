5466c4ed210743dfce7e839b5564c5171e72bc98
unknown <jemahechaj@ut.edu.co>
2019-05-14 13:53:44 -0500

    Finalizado Vue + Ionic4

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,12 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <ion-card>
+    <ion-item>
+      <ion-label position="floating"></ion-label>
+      <ion-input></ion-input>
+    </ion-item>
+  </ion-card>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  }
-}
+export default {};
 </script>
