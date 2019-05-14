2010748adba6537ceeacb83e8d59681141a024a9
unknown <jemahechaj@ut.edu.co>
2019-05-14 14:20:56 -0500

    Calibrando

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,31 @@
 <template>
   <ion-card>
     <ion-item>
-      <ion-label position="floating"></ion-label>
-      <ion-input></ion-input>
+      <ion-label position="floating">Nombre</ion-label>
+      <ion-input @input="greeting = $event.target.value"></ion-input>
     </ion-item>
+    <ion-button color="tertiary" expand="full" @click="handleClick">Hello</ion-button>
   </ion-card>
 </template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      greeting: ""
+    };
+  },
+  methods: {
+    handleClick() {
+      this.$ionic.alertController
+        .create({
+          header: "alert",
+          subHeader: "subtitle",
+          message: this.greeting,
+          buttons: ["Ok"]
+        })
+        .then(a => a.present());
+    }
+  }
+};
 </script>
